85c619c52318585111e55e88ce76c2a505fc146d
binarii <karl50018@gmail.com>
2025-03-04 13:57:55 +0800

    unsloth finetuning

--- gpro.py
+++ gpro.py
@@ -11,9 +11,12 @@ import time
 from datasets import Dataset, load_dataset
 from trl import GRPOConfig, GRPOTrainer
 from unsloth import is_bfloat16_supported
+from unsloth.chat_templates import get_chat_template
+# from unsloth.tokenizer_utils import add_new_tokens
+
 
 _MAX_SEQ_LENGTH = 1024
-_LORA_RANK = 64
+_MAX_LORA_RANK = 64
 
 
 def _train(_args: argparse.Namespace):
@@ -22,67 +25,69 @@ def _train(_args: argparse.Namespace):
         max_seq_length=_MAX_SEQ_LENGTH,
         load_in_4bit=True,
         fast_inference=True,
-        max_lora_rank=_LORA_RANK,
-        gpu_memory_utilization=0.5,
+        max_lora_rank=_MAX_LORA_RANK,
+        gpu_memory_utilization=0.9,
     )
 
     model = FastLanguageModel.get_peft_model(
         model,
-        r=_LORA_RANK,
+        r=_MAX_LORA_RANK,
         # Remove QKVO if out of memory
         target_modules=[
-            "q_proj",
-            "k_proj",
-            "v_proj",
-            "o_proj",
+            # "q_proj",
+            # "k_proj",
+            # "v_proj",
+            # "o_proj",
             "gate_proj",
             "up_proj",
             "down_proj",
         ],
-        lora_alpha=_LORA_RANK,
+        lora_alpha=_MAX_LORA_RANK,
         use_gradient_checkpointing="unsloth",
         random_state=3407,
     )
 
-    SYSTEM_PROMPT = """
-    Respond in the following format:
-    <reasoning>
-    ...
-    </reasoning>
-    <answer>
-    ...
-    </answer>
-    """
+    _SYSTEM_PROMPT = "Respond in the following format: <think>{some thinking}</think> final answer here"
 
-    # ruff: noqa: F841
-    XML_COT_FORMAT = """\
-    <reasoning>
-    {reasoning}
-    </reasoning>
-    <answer>
-    {answer}
-    </answer>
-    """
+    tokenizer = get_chat_template(
+        tokenizer,
+        chat_template="qwen2.5",
+        mapping={
+            "role": "role",
+            "content": "content",
+            "user": "user",
+            "assistant": "assistant",
+        },
+        map_eos_token=True,
+        system_message=_SYSTEM_PROMPT,
+    )
+    tokenizer.pad_token = tokenizer.eos_token
+    # add_new_tokens(model, tokenizer, ["<think>", "</think>"]) ## CUDA OOM
 
     def extract_xml_answer(text: str) -> str:
+        answer = text.split("</think>")[-1]
+        answer = answer.strip()
         answer = text.split("<answer>")[-1]
-        answer = answer.split("</answer>")[0]
+        answer = text.split("</answer>")[0]
         return answer.strip()
 
-    def extract_hash_answer(text: str) -> str | None:
-        if "####" not in text:
-            return None
-        return text.split("####")[1].strip()
+    # def extract_hash_answer(text: str) -> str | None:
+    #     if "####" not in text:
+    #         return None
+    #     return text.split("####")[1].strip()
 
     def get_gsm8k_questions(split="train") -> Dataset:
-        data = load_dataset("openai/gsm8k", "main")[split]
+        # data = load_dataset("openai/gsm8k", "main")[split]
+        data = load_dataset("swulling/gsm8k_chinese")[split]  ## CN
         data = data.map(
             lambda x: {
                 "prompt": [
-                    {"role": "system", "content": SYSTEM_PROMPT},
-                    {"role": "user", "content": x["question"]},
+                    {"role": "system", "content": _SYSTEM_PROMPT},
+                    # {"role": "user", "content": x["question"]},
+                    {"role": "user", "content": x["question_zh-cn"]},  ## CN
                 ],
-                "answer": extract_hash_answer(x["answer"]),
+                "answer": x["answer_only"],  ## CN
+                # "answer": extract_hash_answer(x["answer"]),
             }
         )
         return data
@@ -109,30 +114,31 @@ def _train(_args: argparse.Namespace):
 
     def strict_format_reward_func(completions, **kwargs) -> list[float]:
         """Reward function that checks if the completion has a specific format."""
-        pattern = r"^<reasoning>\n.*?\n</reasoning>\n<answer>\n.*?\n</answer>\n$"
+        pattern = r"^<think>.*?</think>.*?$"
         responses = [completion[0]["content"] for completion in completions]
         matches = [re.match(pattern, r) for r in responses]
         return [0.5 if match else 0.0 for match in matches]
 
     def soft_format_reward_func(completions, **kwargs) -> list[float]:
         """Reward function that checks if the completion has a specific format."""
-        pattern = r"<reasoning>.*?</reasoning>\s*<answer>.*?</answer>"
+        pattern = r"<think>.*?</think>.*?"
         responses = [completion[0]["content"] for completion in completions]
         matches = [re.match(pattern, r) for r in responses]
         return [0.5 if match else 0.0 for match in matches]
 
     def count_xml(text) -> float:
         count = 0.0
-        if text.count("<reasoning>\n") == 1:
+        if text.count("<think>") == 1:
             count += 0.125
-        if text.count("\n</reasoning>\n") == 1:
+        if text.count("</think>") == 1:
             count += 0.125
-        if text.count("\n<answer>\n") == 1:
-            count += 0.125
-            count -= len(text.split("\n</answer>\n")[-1]) * 0.001
-        if text.count("\n</answer>") == 1:
-            count += 0.125
-            count -= (len(text.split("\n</answer>")[-1]) - 1) * 0.001
+            count += len(text.split("</think>")[-1]) * 0.001
+        if text.count("<answer>") > 0:
+            count -= 0.125
+            count -= len(text.split("<answer>")[-1]) * 0.001
+        if text.count("</answer>") > 0:
+            count -= 0.125
+            count -= (len(text.split("</answer>")[-1])) * 0.001
         return count
 
     def xmlcount_reward_func(completions, **kwargs) -> list[float]:
@@ -153,12 +159,12 @@ def _train(_args: argparse.Namespace):
         fp16=not is_bfloat16_supported(),
         per_device_train_batch_size=1,
         gradient_accumulation_steps=1,
-        num_generations=8,
+        num_generations=4,  # Decrease if out of memory
         max_prompt_length=256,
         max_completion_length=200,
         # num_train_epochs = 1,
-        max_steps=250,
-        save_steps=250,
+        max_steps=300,
+        save_steps=300,
         max_grad_norm=0.1,
         report_to="none",
         output_dir="outputs",
@@ -201,13 +207,13 @@ def _train(_args: argparse.Namespace):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--base-model", type=str, default="Qwen2.5-3B-Instruct")
+    parser.add_argument("--base-model", type=str, default="Qwen2.5-7B-Instruct")
     parser.add_argument("--sys-prompt", type=str, default="system_message.md")
     parser.add_argument("--save-lora", action="store_true", default=True)
     parser.add_argument("--save-vllm", action="store_true", default=True)
     parser.add_argument("--save-gguf", action="store_true", default=False)
     parser.add_argument("--saved-path", type=str, default="./models")
-    parser.add_argument("--saved-name", type=str, default="Qwen2.5-3B-GPRO")
+    parser.add_argument("--saved-name", type=str, default="Qwen2.5-7B-GPRO")
     _args = parser.parse_args()
 
     start_time = time.time()
