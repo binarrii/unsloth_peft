57b811c36238d65c10ba9bfcb6fb9c2ae7da5e02
binarii <karl50018@gmail.com>
2025-03-27 16:06:09 +0800

    unsloth finetuning

---
+++ parquet2sql.py
@@ -0,0 +1,22 @@
+from urllib.parse import quote
+
+from datasets import load_dataset
+from sqlalchemy import create_engine
+
+username = quote("root")
+password = quote("devel-root@251")
+host = "10.252.25.251"
+port = 3309
+database = "devel"
+
+engine = create_engine(
+    f"mysql+pymysql://{username}:{password}@{host}:{port}/{database}"
+)
+
+# import pandas as pd
+# df = pd.read_parquet("train.parquet")
+
+dataset = load_dataset("BruceNju/crosswoz-sft", split="train")
+
+df = dataset.to_pandas()
+df.to_sql("crosswoz_sft", con=engine, if_exists="append", index=False)
